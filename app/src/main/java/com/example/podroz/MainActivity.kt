ec11aa748a55392b4c54c0b9de419c539d6aad48
Ok-tvn <oktawiansegieda@gmail.com>
2022-04-11 15:09:52 +0200

    slider

--- app/src/main/java/com/example/podroz/MainActivity.kt
+++ app/src/main/java/com/example/podroz/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.podroz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        lateinit var Suwak: SeekBar
+        lateinit var TexTSuw: TextView
+
+        Suwak = findViewById<SeekBar>(R.id.seekBar)
+        TexTSuw = findViewById<TextView>(R.id.textViewSuwak)
+        var a:Float
+
+        Suwak.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int,fromUser: Boolean) {
+
+                //mapa.setRotation(Suwak.progress.toFloat())
+
+
+                a = (progress.toFloat())
+                a=a/10
+                TexTSuw.text = a.toString()
+
+
+
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+        })
+
     }
 }
